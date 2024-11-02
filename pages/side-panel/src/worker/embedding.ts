e80dd261261144c12ddab77e85374ffc919b5053
huangyuan <xiaoyuan98@126.com>
2024-11-02 17:23:09 +0800

    修复bug

--- pages/side-panel/src/worker/embedding.ts
+++ pages/side-panel/src/worker/embedding.ts
@@ -5,7 +5,7 @@ import { embedding } from '@extension/shared';
 const embeddingText = async (texts: string[]) => {
     await embedding.init()
     const embeddingOutput = await embedding.encode(texts);
-    return { embeddedSentences: embeddingOutput.dataSync(), texts: texts };
+    return { embeddedSentences: embeddingOutput.arraySync(), texts: texts };
 }
 
 workerpool.worker({
