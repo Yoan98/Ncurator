9e9508087215099a7e9d8b717488c1a0ed1a9cfa
huangyuan <xiaoyuan98@126.com>
2024-12-06 18:57:02 +0800

    修改帮助页的跳转

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -134,7 +134,7 @@ const SidePanel = () => {
             setPagePath('/llm-set');
         } else if (key == 3) {
             //打开tab页
-            chrome.tabs.create({ url: chrome.runtime.getURL('new-tab/index.html') });
+            window.open('https://yoan98.github.io/BeCuratorIntroduce/')
         }
     }
     const handleChatHistoryUpdate = (localChatHistory: Chat.LocalHistory[]) => {
