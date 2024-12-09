c7f7c8ea470115336347f4be5034dac2fb824663
huangyuan <xiaoyuan98@126.com>
2024-12-09 18:07:31 +0800

    调整域名

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -134,7 +134,7 @@ const SidePanel = () => {
             setPagePath('/llm-set');
         } else if (key == 3) {
             //打开tab页
-            window.open('https://yoan98.github.io/BeCuratorIntroduce')
+            window.open('https://be-curator-introduce.vercel.app')
         }
     }
     const handleChatHistoryUpdate = (localChatHistory: Chat.LocalHistory[]) => {
